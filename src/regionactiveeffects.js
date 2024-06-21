682e281260c087a621784953b1c1a2c3568ae175
Chris Seieroe <kaelad02@users.noreply.github.com>
2024-06-21 12:26:50 -0700

    Add the statusEffect behavior

--- src/regionactiveeffects.js
+++ src/regionactiveeffects.js
@@ -1,3 +1,75 @@
+const { RegionBehaviorType } = foundry.data.regionBehaviors;
+const { BooleanField, StringField } = foundry.data.fields;
+
 Hooks.once("init", () => {
-  console.log("Hello from RAE");
+  // register the DataModel
+  Object.assign(CONFIG.RegionBehavior.dataModels, {
+    "regionactiveeffects.statusEffect": StatusEffectRegionBehaviorType,
+  });
+
+  // register the Sheet
+  DocumentSheetConfig.registerSheet(
+    RegionBehavior,
+    "regionactiveeffects",
+    foundry.applications.sheets.RegionBehaviorConfig,
+    {
+      types: ["regionactiveeffects.statusEffect"],
+      makeDefault: true,
+    }
+  );
 });
+
+/**
+ * The data model for a behavior that applies a status effect while inside the Region.
+ */
+class StatusEffectRegionBehaviorType extends RegionBehaviorType {
+  static defineSchema() {
+    return {
+      statusId: new StringField({
+        required: true,
+        blank: false,
+        nullable: true,
+        initial: null,
+        // TODO sort choices by label
+        choices: CONFIG.statusEffects.reduce((obj, statusEffect) => {
+          obj[statusEffect.id] = statusEffect.name;
+          return obj;
+        }, {}),
+        label: "RAE.TYPES.statusEffect.FIELDS.statusId.label",
+        hint: "RAE.TYPES.statusEffect.FIELDS.statusId.hint",
+      }),
+      overlay: new BooleanField({
+        initial: false,
+        label: "RAE.TYPES.statusEffect.FIELDS.overlay.label",
+        hint: "RAE.TYPES.statusEffect.FIELDS.overlay.hint",
+      }),
+    };
+  }
+
+  static async #onTokenEnter(event) {
+    // quick data verification
+    const actor = event.data?.token?.actor;
+    if (!actor || !this.statusId) return;
+
+    // only run on triggering user
+    if (!event.user.isSelf) return;
+
+    actor.toggleStatusEffect(this.statusId, { active: true, overlay: this.overlay });
+  }
+
+  static async #onTokenExit(event) {
+    // quick data verification
+    const actor = event.data?.token?.actor;
+    if (!actor || !this.statusId) return;
+
+    // only run on triggering user
+    if (!event.user.isSelf) return;
+
+    actor.toggleStatusEffect(this.statusId, { active: false });
+  }
+
+  static events = {
+    [CONST.REGION_EVENTS.TOKEN_ENTER]: this.#onTokenEnter,
+    [CONST.REGION_EVENTS.TOKEN_EXIT]: this.#onTokenExit,
+  };
+}
