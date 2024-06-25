3b53c6ed8d535abc0f059af7a297887d53290397
Chris Seieroe <kaelad02@users.noreply.github.com>
2024-06-25 15:53:07 -0700

    add activeEffect behavior

--- src/region-active-effects.js
+++ src/region-active-effects.js
@@ -1,11 +1,12 @@
 const { RegionBehaviorType } = foundry.data.regionBehaviors;
-const { BooleanField, StringField } = foundry.data.fields;
+const { BooleanField, DocumentUUIDField, StringField } = foundry.data.fields;
 
 Hooks.once("init", () => {
   // register the DataModel
   Object.assign(CONFIG.RegionBehavior.dataModels, {
     "region-active-effects.statusEffect": StatusEffectRegionBehaviorType,
     "region-active-effects.statusEffectEvents": StatusEffectEventsRegionBehaviorType,
+    "region-active-effects.activeEffect": ActiveEffectRegionBehaviorType,
   });
 
   // register the Sheet
@@ -14,7 +15,11 @@ Hooks.once("init", () => {
     "region-active-effects",
     foundry.applications.sheets.RegionBehaviorConfig,
     {
-      types: ["region-active-effects.statusEffect", "region-active-effects.statusEffectEvents"],
+      types: [
+        "region-active-effects.statusEffect",
+        "region-active-effects.statusEffectEvents",
+        "region-active-effects.activeEffect",
+      ],
       makeDefault: true,
     }
   );
@@ -123,4 +128,68 @@ class StatusEffectEventsRegionBehaviorType extends RegionBehaviorType {
     const active = this.action === "apply" ? true : this.action === "remove" ? false : undefined;
     actor.toggleStatusEffect(this.statusId, { active, overlay: this.overlay });
   }
+}
+
+async function applyEffectToActor(effect, actor, behavior) {
+  // Enable an existing effect on the actor if it came from this behavior
+  const existingEffect = actor.effects.find((e) => e.origin === behavior.uuid);
+  if (existingEffect) {
+    return existingEffect.update({ ...effect.constructor.getInitialDuration(), disabled: false });
+  }
+
+  // Create a new effect on the actor
+  const effectData = {
+    ...effect.toObject(),
+    disabled: false,
+    transfer: false,
+    origin: behavior.uuid,
+  };
+  return ActiveEffect.implementation.create(effectData, { parent: actor });
+}
+
+class ActiveEffectRegionBehaviorType extends RegionBehaviorType {
+  static defineSchema() {
+    return {
+      uuid: new DocumentUUIDField({
+        type: "ActiveEffect",
+        label: "RAE.TYPES.activeEffect.FIELDS.uuid.label",
+        hint: "RAE.TYPES.activeEffect.FIELDS.uuid.hint",
+      }),
+      disable: new BooleanField({
+        initial: false,
+        label: "RAE.TYPES.activeEffect.FIELDS.disable.label",
+        hint: "RAE.TYPES.activeEffect.FIELDS.disable.hint",
+      }),
+    };
+  }
+
+  static async #onTokenEnter(event) {
+    // quick data verification
+    const actor = event.data?.token?.actor;
+    if (!actor || !this.uuid) return;
+
+    // only run on triggering user
+    if (!event.user.isSelf) return;
+
+    const effect = await fromUuid(this.uuid);
+    applyEffectToActor(effect, actor, this.behavior);
+  }
+
+  static async #onTokenExit(event) {
+    // quick data verification
+    const actor = event.data?.token?.actor;
+    if (!actor || !this.uuid) return;
+
+    // only run on triggering user
+    if (!event.user.isSelf) return;
+
+    const existingEffect = actor.effects.find((e) => e.origin === this.behavior.uuid);
+    if (existingEffect && this.disable) existingEffect.update({ disabled: true });
+    else if (existingEffect) existingEffect.delete();
+  }
+
+  static events = {
+    [CONST.REGION_EVENTS.TOKEN_ENTER]: this.#onTokenEnter,
+    [CONST.REGION_EVENTS.TOKEN_EXIT]: this.#onTokenExit,
+  };
 }
